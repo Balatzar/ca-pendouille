2f57f6b5553c57dcccdf13e601994cf2525e9f0a
naamelie <nadalini.amelie@gmail.com>
2015-11-08 15:52:34 +0100

    Comments on files to explain them

--- game.rb
+++ game.rb
@@ -1,4 +1,4 @@
-def game(params)
-	@title = "Jouez !"
-	@word = params[:word]
+def game(params) #mise en place de la fonction game qui appelle un argument
+	@title = "Jouez !" # précise le titre qui sera affiché dans la vue game.erb (titre précisé dans le layout.erb)
+	@word = params[:word] #l'argument params est mis dans une variable @word qui sera appelée dans la vue game.erb
 end
--- pendu.rb
+++ pendu.rb
@@ -1,9 +1,9 @@
-require 'sinatra'
-require './game.rb'
+require 'sinatra' #Appelle fichier (fonctionaité sinatra) pour permettre fonctionnement de l'appli
+require './game.rb' #Appelle le fichier game.rb 
 
-get '/' do
-	@title = "Pendu !"
-	erb :index
+get '/' do #route permettant d'afficher la page index => http method
+	@title = "Pendu !" #@title affiche le titre dans la page index.erb (titre précisé dans le layout.erb)
+	erb :index #précision du template de vue à aller chercher avec le nom de celle-ci
 end
 
 get '/mode_duo' do
@@ -16,7 +16,7 @@ get '/mode_solo' do
 	erb :mode_solo
 end
 
-post '/game' do
-	game(params)
-	erb :game
+post '/game' do #route permettant d'effectuer une action dans la page game 
+	game(params) #l'action effectuée va appeler la fonction "game" sur un argument qu'on lui aura donné
+	erb :game #précision du template utilisé pour afficher le résultat dans la vue mentionnée
 end
